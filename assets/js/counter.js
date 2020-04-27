1217ab90b02523c2dcf92cc0be42c7bd3f38646a
Nick Guggenbuehl <nick.guggenbuehl@gmail.com>
2020-04-27 14:26:09 -0500

    counter

---
+++ assets/js/counter.js
@@ -0,0 +1,30 @@
+
+
+//counter
+
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    // duration: 28800000,
+    duration: 100,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });
+
+
+
+});
